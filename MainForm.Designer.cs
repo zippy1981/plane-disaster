333952a7c6bcdc7ff6a62c4036e32556663a2967
j-pimp <j-pimp@b6e9dbbe-bf1e-0410-883a-b328e32b9170>
2006-11-01 04:10:28 +0000

    MainForm.resources replaced with MainForm.resx. Icon Created with the Gimp.
    git-svn-id: https://plane-disaster.svn.sourceforge.net/svnroot/plane-disaster/trunk@12 b6e9dbbe-bf1e-0410-883a-b328e32b9170

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -51,6 +51,7 @@ namespace PlaneDisaster
 		/// not be able to load this method if it was changed manually.
 		/// </summary>
 		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.lstTables = new System.Windows.Forms.ListBox();
 			this.radText = new System.Windows.Forms.RadioButton();
 			this.radGrid = new System.Windows.Forms.RadioButton();
@@ -204,14 +205,14 @@ namespace PlaneDisaster
 			// newToolStripMenuItem
 			// 
 			this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-			this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.newToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
 			this.newToolStripMenuItem.Text = "&New";
 			this.newToolStripMenuItem.Click += new System.EventHandler(this.menuNew_Click);
 			// 
 			// openToolStripMenuItem
 			// 
 			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-			this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
 			this.openToolStripMenuItem.Text = "&Open";
 			this.openToolStripMenuItem.Click += new System.EventHandler(this.menuOpen_Click);
 			// 
@@ -219,19 +220,19 @@ namespace PlaneDisaster
 			// 
 			this.closeToolStripMenuItem.Enabled = false;
 			this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-			this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.closeToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
 			this.closeToolStripMenuItem.Text = "&Close";
 			this.closeToolStripMenuItem.Click += new System.EventHandler(this.menuClose_Click);
 			// 
 			// toolStripSeparator1
 			// 
 			this.toolStripSeparator1.Name = "toolStripSeparator1";
-			this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+			this.toolStripSeparator1.Size = new System.Drawing.Size(108, 6);
 			// 
 			// exitToolStripMenuItem
 			// 
 			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-			this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
 			this.exitToolStripMenuItem.Text = "E&xit";
 			this.exitToolStripMenuItem.Click += new System.EventHandler(this.menuExit_Click);
 			// 
@@ -359,6 +360,7 @@ namespace PlaneDisaster
 			this.Controls.Add(this.cmdStatus);
 			this.Controls.Add(this.gridResults);
 			this.Controls.Add(this.menuStrip1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MainMenuStrip = this.menuStrip1;
 			this.MinimumSize = new System.Drawing.Size(884, 300);
 			this.Name = "MainForm";
