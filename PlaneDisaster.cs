34ecec6980f656e3cc3ecfdfcca2ea2a8e6a09d1
j-pimp <j-pimp@b6e9dbbe-bf1e-0410-883a-b328e32b9170>
2007-01-09 09:50:49 +0000

    I cant so a svn copy for some reason.
    git-svn-id: https://plane-disaster.svn.sourceforge.net/svnroot/plane-disaster/trunk@33 b6e9dbbe-bf1e-0410-883a-b328e32b9170

---
+++ PlaneDisaster.cs
@@ -0,0 +1,69 @@
+/* 
+ * Copyright 2006 Justin Dearing
+ * 
+ * This file is part of PlaneDisaster.NET.
+ * 
+ * PlaneDisaster.NET is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ * 
+ * PlaneDisaster.NET is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with PlaneDisaster.NET; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/*
+ * Created by SharpDevelop.
+ * Author:		Justin Dearing <zippy1981@gmail.com>
+ * Created:		08/01/2006
+ * Description  This is the presentation code for PlaneDisaster.NET
+ */
+
+ 
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PlaneDisaster
+{
+	/// <summary>
+	/// PlaneDisaster.NET main class.
+	/// </summary>
+	public sealed class PlaneDisaster
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		/// <param name="args">Command line arguments.</param>
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			MainForm frm = new MainForm();
+			/*
+			 * From the infalliable tomes of the msdn http://msdn2.microsoft.com/en-us/library/acy3edy3.aspx
+			 *    Unlike C and C++, the name of the program is not treated as the first command line argument.
+			 *    WHY WHY WHY!!!!!! Lets just have 1 based arrays.
+			 */
+			if (args.Length > 0) {
+				string FileName = args[0];
+				if (File.Exists(FileName)) {
+					frm.OpenDatabaseFile(FileName);
+					frm.Text = string.Format("{0} - ({1}) - PlaneDisaster.NET", System.IO.Path.GetFileName(FileName), FileName);
+					frm.InitContextMenues();
+				} else if (Directory.Exists(Path.GetDirectoryName(FileName))) {
+					frm.NewDatabaseFile(FileName);
+					frm.InitContextMenues();
+				} else {
+					MessageBox.Show(String.Format("File {0} is not a real file.", FileName));
+				}
+			}
+			Application.Run(frm);
+		}
+
+	}
+}
