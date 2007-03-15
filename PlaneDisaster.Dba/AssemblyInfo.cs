635fb838e9a0fe9c10e6caa52a4cbc341a174e31
j-pimp <j-pimp@b6e9dbbe-bf1e-0410-883a-b328e32b9170>
2007-03-15 22:12:20 +0000

    Renamed PlaneDisaster.LIB to PlaneDisaster.Dba. Remives the NSIS package. Fixed some other issues. Altered guids and bumped version to 0.9.3
    git-svn-id: https://plane-disaster.svn.sourceforge.net/svnroot/plane-disaster/trunk@55 b6e9dbbe-bf1e-0410-883a-b328e32b9170

---
+++ PlaneDisaster.Dba/AssemblyInfo.cs
@@ -0,0 +1,50 @@
+/* 
+ * Copyright 2006 Justin Dearing
+ * 
+ * This file is part of PlaneDisaster.NET.
+ * 
+ * PlaneDisaster.NET is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ * 
+ * PlaneDisaster.NET is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with PlaneDisaster.NET; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Information about this assembly is defined by the following
+// attributes.
+//
+// change them to the information which is associated with the assembly
+// you compile.
+
+[assembly: AssemblyTitle("PlaneDisaster.Dba")]
+[assembly: AssemblyDescription("Data Access Layer for PlaneIDsaster Suite")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ZippySoft")]
+[assembly: AssemblyProduct("Plane Disaster")]
+[assembly: AssemblyCopyright("2006 Justin Dearing\nzippy1981@gmail.com")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible(false)]
+
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all values by your own or you can build default build and revision
+// numbers with the '*' character (the default):
+
+[assembly: AssemblyVersion("0.9.3")]
---
+++ PlaneDisaster.Dba/JetSqlUtil.cs
@@ -0,0 +1,152 @@
+/*/ 
+ * Copyright 2006 Justin Dearing
+ * 
+ * This file is part of PlaneDisaster.NET.
+ * 
+ * PlaneDisaster.NET is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ * 
+ * PlaneDisaster.NET is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with PlaneDisaster.NET; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+/*/
+
+/*/
+ * Created by SharpDevelop.
+ * Author:		Justin Dearing <zippy1981@gmail.com>
+ * Date: 8/21/2006
+ * Time: 2:00 AM
+/*/
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace PlaneDisaster.Dba
+{
+	/// <summary>
+	/// JetSQL is the "code name" for the sql engine behind access.
+	/// It's auctually built into windows. Microsoft Access is just a fancy
+	/// front end.
+	/// </summary>
+	public class JetSqlUtil
+	{
+		internal enum ODBC_Constants : int {
+    		ODBC_ADD_DSN = 1,
+    		ODBC_CONFIG_DSN,
+    		ODBC_REMOVE_DSN,
+    		ODBC_ADD_SYS_DSN,
+    		ODBC_CONFIG_SYS_DSN,
+    		ODBC_REMOVE_SYS_DSN,
+    		ODBC_REMOVE_DEFAULT_DSN,
+		}
+		
+		[DllImport("ODBCCP32.DLL",CharSet=CharSet.Unicode, SetLastError=true)]
+		internal static extern int SQLConfigDataSource (int hwndParent, ODBC_Constants fRequest, string lpszDriver, string lpszAttributes);
+		
+		
+		/// <summary>
+		/// Compacts an access database
+		/// </summary>
+		/// <param name="FileName">The name of the databse to compact.</param>
+		public static void CompactMDB (string FileName) {
+			int retCode;
+			string Attributes = 
+				String.Format("COMPACT_DB=\"{0}\" \"{0}\" General\0", FileName);
+			retCode = SQLConfigDataSource
+				(0, ODBC_Constants.ODBC_ADD_DSN, 
+				"Microsoft Access Driver (*.mdb)", Attributes);
+			if (retCode == 0) {
+				throw new ApplicationException("Cannot compact database: " + FileName);
+			}
+		}
+		
+		
+		/// <summary>
+		/// Creates an access database. If the filename specified exists it is 
+		/// overwritten.
+		/// </summary>
+		/// <param name="FileName">The name of the databse to create.</param>
+		public static void CreateMDB (string FileName) {
+			int retCode;
+			if (File.Exists(FileName)) {
+				File.Delete(FileName);
+			}
+			string Attributes = 
+				String.Format("CREATE_DB=\"{0}\" General\0", FileName);
+			retCode = SQLConfigDataSource
+				(0, ODBC_Constants.ODBC_ADD_DSN, 
+				"Microsoft Access Driver (*.mdb)", Attributes);
+			if (retCode == 0) {
+				throw new ApplicationException("Cannot create file: " + FileName);
+			}
+		}
+		
+		
+		/// <summary>
+		/// Creates an Access 95 database. If the filename specified exists it is 
+		/// overwritten.
+		/// </summary>
+		/// <param name="FileName">The name of the databse to create.</param>
+		public static void CreateMDBv3 (string FileName) {
+			int retCode;
+			if (File.Exists(FileName)) {
+				File.Delete(FileName);
+			}
+			string Attributes = 
+				String.Format("CREATE_DBV3=\"{0}\" General\0", FileName);
+			retCode = SQLConfigDataSource
+				(0, ODBC_Constants.ODBC_ADD_DSN, 
+				"Microsoft Access Driver (*.mdb)", Attributes);
+			if (retCode == 0) {
+				throw new ApplicationException("Cannot create file: " + FileName);
+			}
+		}
+		
+		
+		/// <summary>
+		/// Creates an Access 2000 database. If the filename specified exists it is 
+		/// overwritten.
+		/// </summary>
+		/// <param name="FileName">The name of the databse to create.</param>
+		public static void CreateMDBv4 (string FileName) {
+			int retCode;
+			if (File.Exists(FileName)) {
+				File.Delete(FileName);
+			}
+			string Attributes = 
+				String.Format("CREATE_DBV4=\"{0}\" General\0", FileName);
+			retCode = SQLConfigDataSource
+				(0, ODBC_Constants.ODBC_ADD_DSN, 
+				"Microsoft Access Driver (*.mdb)", Attributes);
+			if (retCode == 0) {
+				throw new ApplicationException("Cannot create file: " + FileName);
+			}
+		}
+		
+		
+		
+		/// <summary>
+		/// Repairs an access database
+		/// </summary>
+		/// <param name="FileName">The name of the databse to repair.</param>
+		public static void RepairMDB (string FileName) {
+			int retCode;
+			string Attributes = 
+				String.Format("REPAIR_DB=\"{0}\"\0", FileName);
+			retCode = SQLConfigDataSource
+				(0, ODBC_Constants.ODBC_ADD_DSN, 
+				"Microsoft Access Driver (*.mdb)", Attributes);
+			if (retCode == 0) {
+				throw new ApplicationException("Cannot repair database: " + FileName);
+			}
+		}
+
+	}
+}
