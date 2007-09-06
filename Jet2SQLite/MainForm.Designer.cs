ccc160b270b2a5221534b5b5dbfa2876eda773ae
j-pimp <j-pimp@b6e9dbbe-bf1e-0410-883a-b328e32b9170>
2007-09-06 21:57:07 +0000

    Added Jet2SQLite icon file.
    git-svn-id: https://plane-disaster.svn.sourceforge.net/svnroot/plane-disaster/trunk@72 b6e9dbbe-bf1e-0410-883a-b328e32b9170

--- Jet2SQLite/MainForm.Designer.cs
+++ Jet2SQLite/MainForm.Designer.cs
@@ -53,6 +53,7 @@ namespace PlaneDisaster.Jet2SQLite
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.txtJetSqlFile = new System.Windows.Forms.TextBox();
 			this.cmdBrowseJetSql = new System.Windows.Forms.Button();
 			this.cmdSQLiteFile = new System.Windows.Forms.Button();
@@ -61,24 +62,26 @@ namespace PlaneDisaster.Jet2SQLite
 			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
 			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.txtLog = new System.Windows.Forms.TextBox();
 			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.txtLog = new System.Windows.Forms.TextBox();
 			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// txtJetSqlFile
 			// 
-			this.txtJetSqlFile.Location = new System.Drawing.Point(3, 27);
+			this.txtJetSqlFile.Location = new System.Drawing.Point(4, 33);
+			this.txtJetSqlFile.Margin = new System.Windows.Forms.Padding(4);
 			this.txtJetSqlFile.Name = "txtJetSqlFile";
-			this.txtJetSqlFile.Size = new System.Drawing.Size(486, 20);
+			this.txtJetSqlFile.Size = new System.Drawing.Size(647, 22);
 			this.txtJetSqlFile.TabIndex = 7;
 			// 
 			// cmdBrowseJetSql
 			// 
-			this.cmdBrowseJetSql.Location = new System.Drawing.Point(358, 53);
+			this.cmdBrowseJetSql.Location = new System.Drawing.Point(477, 65);
+			this.cmdBrowseJetSql.Margin = new System.Windows.Forms.Padding(4);
 			this.cmdBrowseJetSql.Name = "cmdBrowseJetSql";
-			this.cmdBrowseJetSql.Size = new System.Drawing.Size(131, 23);
+			this.cmdBrowseJetSql.Size = new System.Drawing.Size(175, 28);
 			this.cmdBrowseJetSql.TabIndex = 8;
 			this.cmdBrowseJetSql.Text = "Browse For &JetSQL File";
 			this.cmdBrowseJetSql.UseVisualStyleBackColor = true;
@@ -86,9 +89,10 @@ namespace PlaneDisaster.Jet2SQLite
 			// 
 			// cmdSQLiteFile
 			// 
-			this.cmdSQLiteFile.Location = new System.Drawing.Point(358, 109);
+			this.cmdSQLiteFile.Location = new System.Drawing.Point(477, 134);
+			this.cmdSQLiteFile.Margin = new System.Windows.Forms.Padding(4);
 			this.cmdSQLiteFile.Name = "cmdSQLiteFile";
-			this.cmdSQLiteFile.Size = new System.Drawing.Size(131, 23);
+			this.cmdSQLiteFile.Size = new System.Drawing.Size(175, 28);
 			this.cmdSQLiteFile.TabIndex = 10;
 			this.cmdSQLiteFile.Text = "Browse For &SQLite File";
 			this.cmdSQLiteFile.UseVisualStyleBackColor = true;
@@ -96,16 +100,18 @@ namespace PlaneDisaster.Jet2SQLite
 			// 
 			// txtSQLiteFile
 			// 
-			this.txtSQLiteFile.Location = new System.Drawing.Point(3, 82);
+			this.txtSQLiteFile.Location = new System.Drawing.Point(4, 101);
+			this.txtSQLiteFile.Margin = new System.Windows.Forms.Padding(4);
 			this.txtSQLiteFile.Name = "txtSQLiteFile";
-			this.txtSQLiteFile.Size = new System.Drawing.Size(486, 20);
+			this.txtSQLiteFile.Size = new System.Drawing.Size(647, 22);
 			this.txtSQLiteFile.TabIndex = 9;
 			// 
 			// cmdConvert
 			// 
-			this.cmdConvert.Location = new System.Drawing.Point(3, 138);
+			this.cmdConvert.Location = new System.Drawing.Point(4, 170);
+			this.cmdConvert.Margin = new System.Windows.Forms.Padding(4);
 			this.cmdConvert.Name = "cmdConvert";
-			this.cmdConvert.Size = new System.Drawing.Size(486, 23);
+			this.cmdConvert.Size = new System.Drawing.Size(648, 28);
 			this.cmdConvert.TabIndex = 11;
 			this.cmdConvert.Text = "&Convert";
 			this.cmdConvert.UseVisualStyleBackColor = true;
@@ -118,7 +124,8 @@ namespace PlaneDisaster.Jet2SQLite
 									this.fileToolStripMenuItem});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(492, 24);
+			this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+			this.menuStrip1.Size = new System.Drawing.Size(656, 26);
 			this.menuStrip1.TabIndex = 12;
 			this.menuStrip1.Text = "menuStrip1";
 			// 
@@ -128,44 +135,45 @@ namespace PlaneDisaster.Jet2SQLite
 			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
 									this.aboutToolStripMenuItem});
 			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-			this.helpToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(48, 22);
 			this.helpToolStripMenuItem.Text = "&Help";
 			// 
 			// aboutToolStripMenuItem
 			// 
 			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
 			this.aboutToolStripMenuItem.Text = "&About";
 			this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItemClick);
-			// 
-			// txtLog
-			// 
-			this.txtLog.Location = new System.Drawing.Point(3, 111);
-			this.txtLog.Name = "txtLog";
-			this.txtLog.ReadOnly = true;
-			this.txtLog.Size = new System.Drawing.Size(352, 20);
-			this.txtLog.TabIndex = 13;
 			// 
 			// fileToolStripMenuItem
 			// 
 			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
 									this.exitToolStripMenuItem});
 			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-			this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(40, 22);
 			this.fileToolStripMenuItem.Text = "&File";
 			// 
 			// exitToolStripMenuItem
 			// 
 			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-			this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
 			this.exitToolStripMenuItem.Text = "&Exit";
 			this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItemClick);
 			// 
+			// txtLog
+			// 
+			this.txtLog.Location = new System.Drawing.Point(4, 137);
+			this.txtLog.Margin = new System.Windows.Forms.Padding(4);
+			this.txtLog.Name = "txtLog";
+			this.txtLog.ReadOnly = true;
+			this.txtLog.Size = new System.Drawing.Size(468, 22);
+			this.txtLog.TabIndex = 13;
+			// 
 			// MainForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(492, 168);
+			this.ClientSize = new System.Drawing.Size(656, 207);
 			this.Controls.Add(this.txtLog);
 			this.Controls.Add(this.cmdConvert);
 			this.Controls.Add(this.cmdSQLiteFile);
@@ -174,7 +182,9 @@ namespace PlaneDisaster.Jet2SQLite
 			this.Controls.Add(this.txtJetSqlFile);
 			this.Controls.Add(this.menuStrip1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MainMenuStrip = this.menuStrip1;
+			this.Margin = new System.Windows.Forms.Padding(4);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "MainForm";
